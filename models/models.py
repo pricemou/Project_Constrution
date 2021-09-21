b3c636241c77c8aa939ae68463d5097819f2359a
Willof Bassanti Bantys <willofgod.wb@gmail.com>
2021-09-21 01:43:29 +0000

    création auto d'entrepot

--- models/models.py
+++ models/models.py
@@ -9,9 +9,27 @@ class ConstructionProjects(models.Model):
     start_date = fields.Date(string="Date de debut")
     construction_type = fields.Many2one(
         comodel_name='construction.type', string='Type de construction')
-    warehouse = fields.Many2one('stock.warehouse', string="Entrepot")
+    warehouse_id = fields.Many2one('stock.warehouse', string="Entrepot")
     transaction_ids = fields.Many2many(
         'stock.picking', relation='transaction', column1='scheduled_date', column2='picking_type_id')
+
+    @api.model
+    def create(self, vals):
+        result = ''
+        wrd_list = vals['name'].split(' ')
+        if len(wrd_list) >= 2:
+            for word in wrd_list:
+                result += str(word[0])
+        else:
+            result = str(vals['name'][:3])
+
+        new_warehouse = self.env['stock.warehouse'].sudo().create(
+            {
+                'name': vals['name'],
+                'code': result.upper()
+            })
+        vals['warehouse_id'] = new_warehouse.id
+        return super(ConstructionProjects, self).create(vals)
 
 
 class ConstructionType(models.Model):
